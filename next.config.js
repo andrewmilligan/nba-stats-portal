c909e8f6b16ae84fa78892c22d906898548d4f79
Andrew Milligan <amilligan@politico.com>
2022-11-29 11:54:23 -0800

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,27 @@
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = (phase) => ({
-  basePath: '/nba',
-  trailingSlash: true,
-  reactStrictMode: true,
-  publicRuntimeConfig: {
-    IS_DEVELOPMENT: phase === PHASE_DEVELOPMENT_SERVER,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/backend/:path*',
-        destination: 'https://d19kaplwqv19rl.cloudfront.net/:path*',
-      },
-    ];
-  },
-});
+const nextConfig = (phase) => {
+  const basePath = '/nba';
+  const deploymentDomain = 'https://milligan.news';
+  return {
+    basePath,
+    trailingSlash: true,
+    reactStrictMode: true,
+    publicRuntimeConfig: {
+      basePath,
+      deploymentDomain,
+      IS_DEVELOPMENT: phase === PHASE_DEVELOPMENT_SERVER,
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/backend/:path*',
+          destination: 'https://d19kaplwqv19rl.cloudfront.net/:path*',
+        },
+      ];
+    },
+  };
+};
 
 module.exports = nextConfig
