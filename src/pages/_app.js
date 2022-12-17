efeb1585f2ee1ffd98e405403467a92a5f0014b2
Andrew Milligan <amilligan@politico.com>
2022-12-17 09:31:28 -0600

    Add Cloudflare analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,21 @@
+import Script from 'next/script';
 import AtomsRoot from 'Atoms/index';
 
 import 'Styles/globals.scss'
 
 const App = function App({ Component, pageProps }) {
   return (
-    <AtomsRoot pageProps={pageProps}>
-      <Component {...pageProps} />
-    </AtomsRoot>
+    <>
+      <AtomsRoot pageProps={pageProps}>
+        <Component {...pageProps} />
+      </AtomsRoot>
+
+      {/* Cloudflare Web Analytics */}
+      <Script
+        src="https://static.cloudflareinsights.com/beacon.min.js"
+        data-cf-beacon='{"token": "483cd5cbee314deb9183b549103b21a0"}'
+      />
+    </>
   );
 };
 
